e0c7fab51e9f6a13daa60ab37c187253692d6d5b
calaw1110 <calaw1110@gmail.com>
2023-04-29 17:30:00 +0800

    求請網址調整成https

--- backend/DatingApp.API/Program.cs
+++ backend/DatingApp.API/Program.cs
@@ -33,7 +33,7 @@ if (app.Environment.IsDevelopment())
 // app.UseAuthorization();
 
 // 調整CORS政策 允許 http://localhost:4200 任何請求
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 
 app.MapControllers();
