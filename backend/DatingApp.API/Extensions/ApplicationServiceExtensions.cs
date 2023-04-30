10e08bf03d02d36c666722ccbf82b912db1a0f0f
calaw1110 <calaw1110@gmail.com>
2023-05-01 00:24:59 +0800

    新增自訂 programs 加入服務的額外方法：加入自定義服務集合、加入Identity服務 修改programs.cs的DI內容 appsettings 加入token key

---
+++ backend/DatingApp.API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,27 @@
+﻿using DatingApp.API.Data;
+using DatingApp.API.Interfaces;
+using DatingApp.API.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Extensions
+{
+	public static class ApplicationServiceExtensions
+	{
+		public static IServiceCollection AddApplicationServices(this IServiceCollection services,IConfiguration config)
+		{
+
+			// 加入資料庫連線設定
+			services.AddDbContext<DatingAppDataContext>(option =>
+			{
+				option.UseSqlite(config.GetConnectionString("DefaultConnection"));
+			});
+
+			// CORS
+			services.AddCors();
+
+			// 加入自訂Service 
+			services.AddScoped<ITokenService, TokenService>();
+			return services;
+		}
+	}
+}
