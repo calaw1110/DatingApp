b3b830c75966e46a199e68d247201ec00a61ca0e
calaw1110 <calaw1110@gmail.com>
2023-05-01 00:34:52 +0800

    補推 IdentityService

--- backend/DatingApp.API/Extensions/IdentityServiceExtensions.cs
+++ backend/DatingApp.API/Extensions/IdentityServiceExtensions.cs
@@ -1,6 +1,26 @@
-﻿namespace DatingApp.API.Extensions
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace DatingApp.API.Extensions
 {
-	public class IdentityServiceExtensions
+	public static class IdentityServiceExtensions
 	{
+		public static IServiceCollection AddIdentityService(this IServiceCollection services, IConfiguration config)
+		{
+
+			services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+			.AddJwtBearer(options =>
+			{
+				options.TokenValidationParameters = new TokenValidationParameters
+				{
+					ValidateIssuerSigningKey = true,
+					IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"])),
+					ValidateIssuer = false,
+					ValidateAudience = false
+				};
+			});
+			return services;
+		}
 	}
 }
