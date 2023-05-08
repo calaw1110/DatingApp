4a099fe8ad28f36f9ab73f677362c921fbc22fe2
calaw1110 <calaw1110@gmail.com>
2023-05-08 22:04:33 +0800

    UserController加入IPhotoService 新增上傳照片以及更新主照片程式

--- backend/DatingApp.API/Controllers/UsersController.cs
+++ backend/DatingApp.API/Controllers/UsersController.cs
@@ -1,5 +1,7 @@
 using AutoMapper;
 using DatingApp.API.DTOs;
+using DatingApp.API.Entities;
+using DatingApp.API.Extensions;
 using DatingApp.API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -13,11 +15,13 @@ namespace DatingApp.API.Controllers
 	{
 		private readonly IUserRepository _userRepository;
 		private readonly IMapper _mapper;
+		private readonly IPhotoService _photoService;
 
-		public UsersController(IUserRepository userRepository, IMapper mapper)
+		public UsersController(IUserRepository userRepository, IMapper mapper, IPhotoService photoService)
 		{
 			this._userRepository = userRepository;
 			this._mapper = mapper;
+			this._photoService = photoService;
 		}
 
 
@@ -39,9 +43,9 @@ namespace DatingApp.API.Controllers
 		public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
 		{
 			// 取得登入驗證資訊
-			var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+			var username = User.GetUsername();
 
-			// 取得登入者資料
+			// 取得登入者詳細資料
 			var user = await _userRepository.GetUserByUsernameAsync(username);
 
 
@@ -58,5 +62,70 @@ namespace DatingApp.API.Controllers
 			// 更新失敗 使用 status code 400 回傳
 			return BadRequest("Failed to update user");
 		}
+
+		[HttpPost("add-photo")]
+		public async Task<ActionResult<PhotoDto>> AddPhoto(IFormFile file)
+		{
+			// 取得登入驗證資訊
+			var username = User.GetUsername();
+
+			// 取得登入者詳細資料
+			var user = await _userRepository.GetUserByUsernameAsync(username);
+
+			if (user == null) return NotFound();
+
+			var result = await _photoService.AddPhotoAsync(file);
+
+			if (result.Error != null) return BadRequest(result.Error.Message);
+
+			var photo = new Photo
+			{
+				Url = result.SecureUrl.AbsoluteUri,
+				PublicId = result.PublicId,
+			};
+
+			if (user.Photos.Count == 0) photo.IsMain = true;
+
+			user.Photos.Add(photo);
+
+			if (await _userRepository.SavaAllAsync())
+			{
+				// status 201 
+				return CreatedAtAction(nameof(GetUsers), new { username = user.UserName }, _mapper.Map<PhotoDto>(photo));
+
+				// status 200
+				// return _mapper.Map<PhotoDto>(photo);
+
+			}
+
+			return BadRequest("Problem adding photo");
+		}
+
+		[HttpPut("set-main-photo/{photoId}")]
+		public async Task<ActionResult> SetMainPhoto(int photoId)
+		{
+			var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
+
+			if (user == null) return NotFound();
+
+			var photo = user.Photos.FirstOrDefault(x => x.Id == photoId);
+
+			if (photo == null) return NotFound();
+
+			if (photo.IsMain) return BadRequest("this is already your main photo");
+
+			var currentMain = user.Photos.FirstOrDefault(x => x.IsMain);
+
+			if (currentMain != null) currentMain.IsMain = false;
+			photo.IsMain = true;
+
+			if (await _userRepository.SavaAllAsync()) return NoContent();
+
+			return BadRequest("Problem setting th main photo");
+		}
+
+
+
+
 	}
 }
