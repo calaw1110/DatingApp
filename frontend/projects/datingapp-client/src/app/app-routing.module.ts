cafb092e5efa5f2db351ad413da51f47058571e9
calaw1110 <calaw1110@gmail.com>
2023-05-08 15:21:13 +0800

    調整錯誤資料夾內容

--- frontend/projects/datingapp-client/src/app/app-routing.module.ts
+++ frontend/projects/datingapp-client/src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
+import { PreventUnsavedChangedsGuard } from './_guards/prevent-unsaved-changeds.guard';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { MerberListComponent } from './members/merber-list/merber-list.component';
-import { MerberDetailComponent } from './members/merber-detail/merber-detail.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AuthGuard } from './_guards/auth.guard';
 import { TestErrorComponent } from './errors/test-error/test-error.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
@@ -17,8 +19,9 @@ const routes: Routes = [
         runGuardsAndResolvers: 'always',
         canActivate: [AuthGuard],
         children: [
-            { path: 'members', component: MerberListComponent },
-            { path: 'members/:username', component: MerberDetailComponent },
+            { path: 'members', component: MemberListComponent },
+            { path: 'members/:username', component: MemberDetailComponent },
+            { path: 'member/edit', component: MemberEditComponent, canDeactivate: [PreventUnsavedChangedsGuard] },
             { path: 'lists', component: ListsComponent },
             { path: 'messages', component: MessagesComponent },
         ]
--- frontend/projects/datingapp-client/src/app/app.module.ts
+++ frontend/projects/datingapp-client/src/app/app.module.ts
@@ -9,8 +9,8 @@ import { NavComponent } from './nav/nav.component';
 import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
-import { MerberListComponent } from './members/merber-list/merber-list.component';
-import { MerberDetailComponent } from './members/merber-detail/merber-detail.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { SharedModule } from './_modules/shared.module';
@@ -20,20 +20,23 @@ import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
 import { MemberCardComponent } from './members/member-card/member-card.component';
 import { JwtInterceptor } from './_interceptors/jwt.interceptor';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { LoadingInterceptor } from './_interceptors/loading.interceptor';
 @NgModule({
     declarations: [
         AppComponent,
         NavComponent,
         HomeComponent,
         RegisterComponent,
-        MerberListComponent,
-        MerberDetailComponent,
+        MemberListComponent,
+        MemberDetailComponent,
         ListsComponent,
         MessagesComponent,
         TestErrorComponent,
         NotFoundComponent,
         ServerErrorComponent,
         MemberCardComponent,
+        MemberEditComponent,
     ],
     imports: [
         BrowserModule,
@@ -45,7 +48,8 @@ import { JwtInterceptor } from './_interceptors/jwt.interceptor';
     ],
     providers: [
         { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
-        { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }
+        { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true },
     ],
     bootstrap: [AppComponent],
 })
