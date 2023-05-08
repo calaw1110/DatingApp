054c6173b365ee34d071805c7234154e29765113
calaw1110 <calaw1110@gmail.com>
2023-05-08 15:28:30 +0800

    改寫 memberService,將取得會員資料 存放於service 避免不必要的request請求資料 調整使用getMembers 服務的頁面

--- frontend/projects/datingapp-client/src/app/members/member-list/member-list.component.ts
+++ frontend/projects/datingapp-client/src/app/members/member-list/member-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MembersService } from '../../_services/members.service';
 import { Member } from '../../_models/member';
+import { Observable } from 'rxjs';
 
 @Component({
     selector: 'app-member-list',
@@ -8,17 +9,13 @@ import { Member } from '../../_models/member';
     styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
-    members: Member[] = [];
+    members$: Observable<Member[]> | undefined;
     constructor(private membersService: MembersService) { }
 
     ngOnInit(): void {
-        this.loadMembers()
+        this.members$ = this.membersService.getMembers();
     }
 
-    loadMembers() {
-        this.membersService.getMembers().subscribe({
-            next: members => this.members = members,
-        })
-    }
+
 
 }
