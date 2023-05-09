d6830084f6069d785f22830aa0ad8c101b3587d7
calaw1110 <calaw1110@gmail.com>
2023-05-09 14:50:33 +0800

    調整accountController 回傳資料

--- backend/DatingApp.API/Controllers/AccountController.cs
+++ backend/DatingApp.API/Controllers/AccountController.cs
@@ -15,13 +15,15 @@ namespace DatingApp.API.Controllers
 	public class AccountController : BaseApiController
 	{
 		private readonly DatingAppDataContext _context;
+		private readonly IUserRepository _userRepository;
 
 		public ITokenService _tokenService { get; }
 
-		public AccountController(DatingAppDataContext context,ITokenService tokenService)
+		public AccountController(DatingAppDataContext context, ITokenService tokenService,IUserRepository userRepository)
 		{
 			this._context = context;
 			this._tokenService = tokenService;
+			this._userRepository = userRepository;
 		}
 		[HttpPost("register")] // POST: /api/account/register
 		public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
@@ -40,25 +42,32 @@ namespace DatingApp.API.Controllers
 
 			_context.Users.Add(user);
 			await _context.SaveChangesAsync();
-			return new UserDto { Username=user.UserName,Token = _tokenService.CreateToken(user)};
+			return new UserDto { Username = user.UserName, Token = _tokenService.CreateToken(user) };
 		}
 
 		[HttpPost("login")]
 		public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
 		{
-			var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+			var user = await _userRepository.GetUserByUsernameAsync(loginDto.Username);
+
 			if (user == null) return Unauthorized("invalid username");
 
 			using var hmac = new HMACSHA512(user.PasswordSalt);
 			var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
 
-			for(int i = 0; i < computedHash.Length; i++)
+			for (int i = 0; i < computedHash.Length; i++)
 			{
-				if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("invalid password");
+				if (computedHash[i] != user.PasswordHash[i])
+					return Unauthorized("invalid password");
 			}
 
 
-			return new UserDto { Username = user.UserName, Token = _tokenService.CreateToken(user) };
+			return new UserDto
+			{
+				Username = user.UserName,
+				Token = _tokenService.CreateToken(user),
+				PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
+			};
 		}
 
 
