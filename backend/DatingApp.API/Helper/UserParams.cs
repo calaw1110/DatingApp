2d9aebecc3c38015a77b260c19903eff39bd39b9
calaw1110 <calaw1110@gmail.com>
2023-05-10 00:52:25 +0800

    新增 HttpExtensions 、新增PagedList 類別、新增PageinationHeader 類別、新增分頁查詢類別

---
+++ backend/DatingApp.API/Helper/UserParams.cs
@@ -0,0 +1,29 @@
+﻿namespace DatingApp.API.Helper
+{
+	public class UserParams
+	{
+		private const int MaxPageSize = 50;
+		public int PageNumber { get; set; } = 1;
+		private int _pageSize = 10;
+
+		public int PageSize
+		{
+			get { return _pageSize; }
+
+			set
+			{
+				if (value > MaxPageSize)
+				{
+					_pageSize = MaxPageSize;
+				}
+				else
+				{
+					_pageSize = value;
+				}
+			}
+
+			// 語法簡化
+			//set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+		}
+	}
+}
