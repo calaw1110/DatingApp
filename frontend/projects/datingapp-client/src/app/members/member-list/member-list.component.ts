8b7cadbbf2eb7ec71ae5fa3783b37dca02e02552
calaw1110 <calaw1110@gmail.com>
2023-05-10 01:26:35 +0800

    加入ngx-bootstrap PaginationModule，member-list 調整

--- frontend/projects/datingapp-client/src/app/members/member-list/member-list.component.ts
+++ frontend/projects/datingapp-client/src/app/members/member-list/member-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { MembersService } from '../../_services/members.service';
 import { Member } from '../../_models/member';
 import { Observable } from 'rxjs';
+import { Pagination } from '../../_models/Pagination';
+import { PageChangedEvent } from 'ngx-bootstrap/pagination';
 
 @Component({
     selector: 'app-member-list',
@@ -9,13 +11,30 @@ import { Observable } from 'rxjs';
     styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
-    members$: Observable<Member[]> | undefined;
+
+    members: Member[] | undefined;
+    pagination: Pagination | undefined;
+    pageNumber = 1;
+    pageSize = 5;
     constructor(private membersService: MembersService) { }
 
     ngOnInit(): void {
-        this.members$ = this.membersService.getMembers();
+        this.loadMembers();
     }
 
-
-
+    loadMembers() {
+        this.membersService.getMembers(this.pageNumber, this.pageSize).subscribe({
+            next: response => {
+                if (response.result && response.pagination) {
+                    this.members = response.result;
+                    this.pagination = response.pagination;
+                }
+            }
+        })
+    }
+    pageChaged(event: PageChangedEvent) {
+        console.log(event);
+         this.pageNumber = event.page;
+         this.loadMembers()
+    }
 }
