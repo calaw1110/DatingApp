dbb12c17a864233ea31879e36d193a826ab16696
calaw1110 <calaw1110@gmail.com>
2023-05-09 22:11:03 +0800

    調整 photo-editor

--- frontend/projects/datingapp-client/src/app/members/photo-editor/photo-editor.component.ts
+++ frontend/projects/datingapp-client/src/app/members/photo-editor/photo-editor.component.ts
@@ -79,9 +79,15 @@ export class PhotoEditorComponent implements OnInit {
             file.withCredentials = false;
         }
         this.uploader.onSuccessItem = (item, response, status, headers) => {
+            console.log(item, response, status, headers);
             if (response) {
-                const photo = JSON.parse(response);
+                const photo: Photo = JSON.parse(response);
                 this.member?.photos.push(photo);
+                if (photo.isMain && this.user && this.member) {
+                    this.user.photoUrl = photo.url;
+                    this.member.photoUrl = photo.url;
+                    this.accountService.setCurrentUser(this.user);
+                }
             }
         }
     }
