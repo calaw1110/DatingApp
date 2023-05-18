6808d85e309ca1a685fb3bb254027de52e0eade1
calaw1110 <calaw1110@gmail.com>
2023-05-19 03:23:38 +0800

    加入即時聊天室功能，重構相關程式

--- frontend/projects/datingapp-client/src/app/members/member-messages/member-messages.component.ts
+++ frontend/projects/datingapp-client/src/app/members/member-messages/member-messages.component.ts
@@ -15,7 +15,7 @@ export class MemberMessagesComponent implements OnInit {
 
     messageContent!: string;
     loading: boolean = false;
-    constructor(private messageService: MessageService) { }
+    constructor(public messageService: MessageService) { }
 
     ngOnInit(): void {
         console.log('messages1', this.messages);
@@ -27,12 +27,9 @@ export class MemberMessagesComponent implements OnInit {
     sendMessage(): void {
         if (!this.username) return; // 若沒有使用者名稱，則不進行發送訊息的動作
         this.loading = true; // 設定載入狀態為 true
-        this.messageService.sendMessage(this.username, this.messageContent).subscribe({
-            next: message => {
-                this.messages.push(message); // 將新訊息加入訊息列表
-                this.messageForm?.reset(); // 重設表單
-                this.loading = false; // 設定載入狀態為 false
-            }
-        });
+        this.messageService.sendMessage(this.username, this.messageContent).then(() => {
+            this.messageForm?.reset();
+            this.loading = false;
+        })
     }
 }
