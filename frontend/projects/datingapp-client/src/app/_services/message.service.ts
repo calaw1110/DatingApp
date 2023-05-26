f04ddf18aa21d1977a1772e8368871f95a5aab9c
calaw1110 <calaw1110@gmail.com>
2023-05-26 22:09:17 +0800

    bug fix

--- frontend/projects/datingapp-client/src/app/_services/message.service.ts
+++ frontend/projects/datingapp-client/src/app/_services/message.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
+import { HttpTransportType, HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
 import { BehaviorSubject, take } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { Group } from '../_models/Group';
@@ -19,28 +19,26 @@ export class MessageService {
     private hubConnection?: HubConnection;
     private messageThreadSource = new BehaviorSubject<Message[]>([]);
     messageThread$ = this.messageThreadSource.asObservable();
-    /** 建構式 */
+
     constructor(private http: HttpClient, private busyService: BusyService) { }
 
     createHubConnection(user: User, otherUsername: string) {
         this.busyService.busy();
         this.hubConnection = new HubConnectionBuilder()
             .withUrl(this.hubUrl + 'message?user=' + otherUsername, {
-                accessTokenFactory: () => user.token
+                accessTokenFactory: () => user.token,
+                transport: HttpTransportType.WebSockets
             })
             .withAutomaticReconnect()
-            .build()
+            .build();
 
         this.hubConnection.start()
-            .catch(
-                error => console.log(error)
-            ).finally(() => {
-                this.busyService.idle()
-            });
+            .catch(error => console.log(error))
+            .finally(() => this.busyService.idle());
 
-        this.hubConnection.on('ReceiveMessageThread', message => {
-            this.messageThreadSource.next(message);
-        });
+        this.hubConnection.on('ReceiveMessageThread', messages => {
+            this.messageThreadSource.next(messages);
+        })
 
         this.hubConnection.on('UpdatedGroup', (group: Group) => {
             if (group.connections.some(x => x.username === otherUsername)) {
@@ -50,75 +48,45 @@ export class MessageService {
                             if (!message.dateRead) {
                                 message.dateRead = new Date(Date.now())
                             }
-                        });
+                        })
                         this.messageThreadSource.next([...messages]);
                     }
-                });
+                })
             }
         })
-
 
         this.hubConnection.on('NewMessage', message => {
             this.messageThread$.pipe(take(1)).subscribe({
                 next: messages => {
                     this.messageThreadSource.next([...messages, message])
-                    console.log("NewMessage", this.messageThreadSource);
                 }
             })
-        });
+        })
     }
 
     stopHubConnection() {
-        console.log("stopHubConnection");
         if (this.hubConnection) {
             this.messageThreadSource.next([]);
-            this.hubConnection?.stop()
-                .catch(error => console.log(error))
+            this.hubConnection.stop();
         }
     }
 
-    /**
-     * 取得對話訊息
-     * @param {number} pageNumber 頁碼
-     * @param {number} pageSize 顯示比數
-     * @param {string} container 信件容器
-     * @return {*} 含分頁訊息的Response
-     */
     getMessages(pageNumber: number, pageSize: number, container: string) {
         let params = getPaginationHeaders(pageNumber, pageSize);
         params = params.append('Container', container);
         return getPaginatedResult<Message[]>(this.baseUrl + 'messages', params, this.http);
     }
 
-    /**
-     * 取得指定用戶名相關的訊息Thread
-     * @param {string} username - 用戶名
-     * @returns {Observable<Message[]>} 特定用戶Thread的可觀察物件
-     */
     getMessageThread(username: string) {
         return this.http.get<Message[]>(this.baseUrl + 'messages/thread/' + username);
     }
 
-    /**
-    * 發送訊息給指定的使用者。
-    * @param username 目標使用者名稱。
-    * @param content 訊息內容。
-    * @returns 一個 Promise，表示發送訊息的非同步操作結果。
-    */
     async sendMessage(username: string, content: string) {
-        // 使用MessageHub連線執行 'SendMessage' 方法
-        // 傳遞一個物件作為參數，包含目標使用者名稱和訊息內容
         return this.hubConnection?.invoke('SendMessage', { recipientUsername: username, content })
-            .catch(error => console.log('SendMessage', error))
+            .catch(error => console.log(error));
     }
 
-
-    /**
-     * 刪除指定ID的訊息
-     * @param {number} id - 訊息ID
-     * @returns {Observable<any>} 表示操作結果的可觀察物件
-     */
     deleteMessage(id: number) {
-        return this.http.delete(this.baseUrl + 'messages/' + id)
+        return this.http.delete(this.baseUrl + 'messages/' + id);
     }
 }
--- frontend/projects/datingapp-client/src/app/members/member-messages/member-messages.component.ts
+++ frontend/projects/datingapp-client/src/app/members/member-messages/member-messages.component.ts
@@ -1,6 +1,5 @@
 import { ChangeDetectionStrategy, Component, Input, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { Message } from '../../_models/message';
 import { MessageService } from '../../_services/message.service';
 
 @Component({
@@ -12,25 +11,19 @@ import { MessageService } from '../../_services/message.service';
 export class MemberMessagesComponent implements OnInit {
     @ViewChild('messageForm') messageForm?: NgForm
     @Input() username?: string;
-    @Input() messages: Message[] = [];
+    messageContent = '';
+    loading = false;
 
-    messageContent!: string;
-    loading: boolean = false;
     constructor(public messageService: MessageService) { }
 
     ngOnInit(): void {
     }
 
-    /**
-     * 發送訊息
-     */
-    sendMessage(): void {
-        if (!this.username) return; // 若沒有使用者名稱，則不進行發送訊息的動作
-        this.loading = true; // 設定載入狀態為 true
+    sendMessage() {
+        if (!this.username) return;
+        this.loading = true;
         this.messageService.sendMessage(this.username, this.messageContent).then(() => {
             this.messageForm?.reset();
-        }).finally(() => {
-            this.loading = false;
-        })
+        }).finally(() => this.loading = false);
     }
 }
