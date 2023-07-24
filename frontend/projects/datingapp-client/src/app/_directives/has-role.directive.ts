9c33ccf289daeddd0325ad4c50dd728bd32d1d13
calaw1110 <calaw1110@gmail.com>
2023-07-25 00:25:15 +0800

    refactor: 加入註解

--- frontend/projects/datingapp-client/src/app/_directives/has-role.directive.ts
+++ frontend/projects/datingapp-client/src/app/_directives/has-role.directive.ts
@@ -10,7 +10,6 @@ export class HasRoleDirective {
     @Input() appHasRole: string[] = [];
     user: User = {} as User;
 
-
     constructor(private viewContainerRef: ViewContainerRef, private templateRef: TemplateRef<any>,
         private accountService: AccountService) {
         this.accountService.currentUser$.pipe(take(1)).subscribe({
@@ -19,10 +18,14 @@ export class HasRoleDirective {
             }
         })
     }
+
     ngOnInit(): void {
+        // 在指令初始化時進行檢查，檢查當前使用者是否具有指定角色
         if (this.user.roles.some(r => this.appHasRole.includes(r))) {
+            // 若使用者具有指定角色，則將對應的模板加入視圖容器中
             this.viewContainerRef.createEmbeddedView(this.templateRef);
         } else {
+            // 若使用者不具有指定角色，則清空視圖容器，不顯示相關內容
             this.viewContainerRef.clear();
         }
     }
