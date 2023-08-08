764244a01843650b5c8b2886be453bac8348e23a
calaw1110 <calaw1110@gmail.com>
2023-08-08 11:59:47 +0800

    refactor : navbar

--- frontend/projects/datingapp-client/src/app/_modules/shared.module.ts
+++ frontend/projects/datingapp-client/src/app/_modules/shared.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 import { FileUploadModule } from 'ng2-file-upload';
 import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ModalModule } from 'ngx-bootstrap/modal';
@@ -11,37 +12,38 @@ import { TabsModule } from 'ngx-bootstrap/tabs';
 import { NgxSpinnerModule } from "ngx-spinner";
 import { TimeagoModule } from 'ngx-timeago';
 import { ToastrModule } from 'ngx-toastr';
-
 @NgModule({
-    declarations: [],
-    imports: [
-        CommonModule,
-        BsDropdownModule.forRoot(),
-        ToastrModule.forRoot({ positionClass: 'toast-bottom-right' }), // ToastrModule added
-        TabsModule.forRoot(),
-        NgxGalleryModule,
-        NgxSpinnerModule.forRoot({
-            type: 'line-scale-pulse-out-rapid'
-        }),
-        FileUploadModule,
-        BsDatepickerModule.forRoot(),
-        PaginationModule.forRoot(),
-        ButtonsModule.forRoot(),
-        TimeagoModule.forRoot(),
-        ModalModule.forRoot(),
-    ],
-    exports: [
-        BsDropdownModule,
-        ToastrModule,
-        TabsModule,
-        NgxGalleryModule,
-        NgxSpinnerModule,
-        FileUploadModule,
-        BsDatepickerModule,
-        PaginationModule,
-        ButtonsModule,
-        TimeagoModule,
-        ModalModule,
-    ]
+  declarations: [],
+  imports: [
+    CommonModule,
+    BsDropdownModule.forRoot(),
+    ToastrModule.forRoot({ positionClass: 'toast-bottom-right' }), // ToastrModule added
+    TabsModule.forRoot(),
+    NgxGalleryModule,
+    NgxSpinnerModule.forRoot({
+      type: 'line-scale-pulse-out-rapid'
+    }),
+    FileUploadModule,
+    BsDatepickerModule.forRoot(),
+    PaginationModule.forRoot(),
+    ButtonsModule.forRoot(),
+    TimeagoModule.forRoot(),
+    ModalModule.forRoot(),
+    CollapseModule.forRoot(),
+  ],
+  exports: [
+    BsDropdownModule,
+    ToastrModule,
+    TabsModule,
+    NgxGalleryModule,
+    NgxSpinnerModule,
+    FileUploadModule,
+    BsDatepickerModule,
+    PaginationModule,
+    ButtonsModule,
+    TimeagoModule,
+    ModalModule,
+    CollapseModule,
+  ]
 })
 export class SharedModule { }
